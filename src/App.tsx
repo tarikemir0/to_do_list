ecde289d6f47b2aa52d7d83bfce533f69bf8e812
Tarık Emir <trkygl1900@gmail.com>
2024-09-16 16:20:52 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { dummyData } from "./data/todos"
+import AddTodoForm from "./components/AddtodoForm";
+import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
+
+function App() {
+  const [todos, setTodos] = useState(dummyData)
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) => 
+    prevTodos.map(todo => (
+      todo.id === id ? {...todo, completed} : todo
+    ))
+    )
+  }
+
+  function addTodo(title:string) {
+    setTodos(prevTodos => [
+      {
+        id: Date.now(),
+        title,
+        completed: false
+      },
+      ...prevTodos
+    ])
+  }
+
+  function deleteTodo(id: number) {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+  function deleteAllCompletedTodos() {
+    setTodos(prevTodos => prevTodos.filter(todo => !todo.completed))
+  }
+
+  return (
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
+      <h1 className="font-bold text-3xl text-center">Yapılacaklar Listem</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm
+        onSubmit={addTodo}
+        />
+        <TodoList 
+        todos={todos} 
+        onCompletedChange={setTodoCompleted}
+        onDelete={deleteTodo}
+        />
+      </div>
+      <TodoSummary
+      todos={todos}
+      deleteAllCompleted={deleteAllCompletedTodos}
+      />
+    </main>
+  )
+}
+
+export default App
---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+import { Todo } from "../types/todo"
+
+8
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "İşe Buluncak",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Evlenilecek",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Saygınlık",
+        completed: false,
+    }
+]
